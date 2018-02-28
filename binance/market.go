ede93750ab7b87e75b25076cbb7e095cb0685a1f
pcslaver <2193240+pcslaver@users.noreply.github.com>
2018-02-28 12:41:26 -0400

    cambio menor de variable reqUrl a reqURL

--- binance/market.go
+++ binance/market.go
@@ -58,7 +58,7 @@ func (b *Binance) GetKlines(q KlineQuery) (klines []Kline, err error) {
 		reqURL = fmt.Sprintf("api/v1/klines?symbol=%s&interval=%s&limit=%d", q.Symbol, q.Interval, q.Limit)
 	}
 
-	_, err = b.client.do("GET", reqUrl, "", false, &klines)
+	_, err = b.client.do("GET", reqURL, "", false, &klines)
 	if err != nil {
 		return
 	}
